1a28b44942809fe36d70113c3a9b7e896ce52fef
kistref <zoltan.varga@doctoral.uj.edu.pl>
2024-11-24 16:29:40 +0100

    changes

--- agent.py
+++ agent.py
@@ -24,7 +24,6 @@ class Agent(ABC):
     
     def __init__(self, id, start_time, origin, destination):
         self.id = id
-
         self.start_time = start_time
         self.origin = origin
         self.destination = destination
@@ -44,226 +43,136 @@ class Agent(ABC):
 
 class HumanAgent(Agent):
 
-    def __init__(self, id, start_time, origin, destination, params, initial_knowledge, action_space_size, human_noise, mutate_to=None):
+    def __init__(self, id, start_time, origin, destination, params, initial_knowledge, action_space_size, **kwargs,):
         super().__init__(id, start_time, origin, destination)
 
-        self.kind = kc.TYPE_HUMAN
-        self.mutate_to = mutate_to
+        #simulation-params
+
+        self.kind = 'Human'
         self.action_space = action_space_size
-        self.stored_utilities = list(np.zeros(self.action_space,dtype=int)) #RK: This shall be generic, not 3 elements (np.zeros(kc.ACTION_SPACE) - or sth like this)
-        self.act_type = params[kc.ACT_TYPE]
-        self.type = params[kc.LEARNING_TYPE]
-        #self.alpha_nulla = params[kc.ALPHA_NULLA]
-        #self.alpha_j = params[kc.ALPHA_J] #RK: How about varying weights for weighted average?
-        self.remember = params[kc.REMEMBER]
-        self.delta = params[kc.DELTA]
-        self.greedy = params[kc.GREEDY]
-        self.test = id
-        self.old_c_hat = [0]
+        self.stored_utilities = list(np.zeros(self.action_space,dtype=int))
+        self.last_reward = None
+
+
+        #Cost-params
+        self.remember = int(kwargs['remember'])
+        self.gamma_c = kwargs['gamma_c']
+        self.old_c_hat = [0,0]
+        self.cost = np.array(initial_knowledge, dtype=float)
+        self.days = [[] for _ in range(action_space_size)]
+
+        #Utility-params
+        self.type = kwargs['learning_type']
+        self.beta = params['beta']
+        self.gamma_u = kwargs['gamma_u']
+        self.greedy = kwargs['greedy']
+
+        #Noise-params
+        self.noise_alpha = kwargs['noise_alpha']
+        self.stored_noise = None
+        self.human_noise = np.random.gumbel() #RK: This shall be generalized towards 'k'-paths noise at init.
+        self.noise_taste = kwargs['noise_taste']
+        self.noise_random = kwargs['noise_random']
+
+        #Alpha values base on learning type
 
         if self.type == 'culo': #this is redundant, this can be hanlded directly at the initialization via kc.PARAMS, no need to do it here
-            self.alpha_nulla = 1
-            self.alpha_j = params[kc.ALPHA_J]
+            self.alpha_zero = 1
+            self.alpha_j = params['alpha_j']
         elif self.type == 'markow':
-            self.alpha_nulla = params[kc.ALPHA_NULLA]
-            self.alpha_j = 1 - self.alpha_nulla
+            self.alpha_zero = params['alpha_zero']
+            self.alpha_j = 1 - self.alpha_zero
         elif self.type == 'weight':
-            self.alpha_nulla = 0
-            self.alpha_j = params[kc.ALPHA_J]
+            self.alpha_zero = 0
+            self.alpha_j = params['alpha_j']
         else:
             print('Correct model definition missing - default markow')
-            self.alpha_nulla = params[kc.ALPHA_NULLA]
-            self.alpha_j = 1 - self.alpha_nulla
+            self.alpha_zero = params['alpha_zero']
+            self.alpha_j = 1 - self.alpha_zero
 
-        beta_randomness = params[kc.BETA_RANDOMNESS]
-        self.beta = params[kc.BETA]
-        self.alpha = params[kc.ALPHA]
-        self.mu = params[kc.MU]
-        self.noise_alpha = params[kc.NOISE_ALPHA]
-        self.stored_noise = None
-        self.randomness = params[kc.RANDOMNES]
 
-        self.human_noise = human_noise #RK: This shall be generalized towards 'k'-paths noise at init.
-
-        self.cost = np.array(initial_knowledge, dtype=float)
-        self.days = list(np.zeros(action_space_size))
-        #for i in range(self.action_space): #RK: this shall be a param - action_space, not hard_coded to "3"
-            #self.days.append([])
-
+        #Creating memory for Weigted model
         self.create_memory()
+
+        #Create noise for routes
         self.route_taste = self.taste_noise()
-
-    
-    def make_noise(self):
-
-        noises = self.noise_alpha * self.human_noise + (self.noise_taste) * self.route_taste + self.noise_random + np.random.gumbel(size=self.action_space) #RK: This shall be generalized to noise for each actin for each day
-        
-        self.stored_noises = noises
     
     def act(self, state):  
         """ 
         the implemented dummy logit model for route choice, make it more generate, calculate in graph levelbookd
         """
-        #if self.act_type == 'test':
-        #    utilities = list(map(lambda x: np.exp(x * self.beta), self.cost))
-        #    prob_dist = [self.calculate_prob(utilities, j) for j in range(len(self.cost))]
-        #elif self.act_type == 'no_test':
-        #print('before:',self.test,self.utility)
+
         self.make_noise()
-        #noises = self.make_noise() #RK: Make noise for each path
+
         utilities = list(map(lambda x,y: ((self.beta * x) + y), self.cost,self.stored_noises)) #RK: here to do: self.noise[k]
-        #else:
-        #    raise ValueError("Define Act type")
 
-        #self.utility = utilities
-        
-        #prob_dist = [self.calculate_prob(utilities, j) for j in range(len(self.cost))]
-        #action = np.random.choice(list(range(len(self.cost))), p=prob_dist)
-        #if self.act_type == 'test':
-        #    action = np.random.choice(list(range(len(self.cost))), p=prob_dist) 
-        #else:
-
-        if abs(self.old_c_hat[-2] - self.old_c_hat[-1]) >= self.gamma_u:
+        if abs(self.old_c_hat[-2] - self.old_c_hat[-1]) > self.gamma_u:
 
             p = np.random.random()
 
             if p < self.greedy:
 
-                action = np.random.randint(low=0,high=len(self.stored_utilities)) #RK: First bounded rationality, then greedy.
+                action = np.random.randint(low=0,high=len(self.action_space)) #RK: First bounded rationality, then greedy.
 
             else:
 
-                action =  np.argmin(utilities)
+                action = np.argmin(utilities)
                 self.stored_utilities = utilities
-
-            #if (self.utility.index(min(self.utility))-utilities.index(min(utilities)))**2 < self.delta:  #RK: Normalize utilities (a-b/a) and get rid of squer (unless found reference that this is advocated)
-
-            #    action = self.utility.index(min(self.utility))
-            #    self.utility = self.utility # redundant
-
-            #else:
-
-            #    action =  utilities.index(min(utilities))
-            #    self.utility = utilities  # RK: confusing, why you mix singular (utility) with plural (utilities) - change names.
-                #why here you save the latest utilites with noise and not in the case when you did not updated? Please discuss and check.
-
         
-        #print('after:',self.test,self.utility)
+        else:
+
+            action = np.argmin(self.stored_utilities)
+
 
         return action       
 
 
-    def learn(self, action, reward, observation):
-        #if self.type == 'culo': #this is redundant, this can be hanlded directly at the initialization via kc.PARAMS, no need to do it here
-        #    alpha_nulla = 1
-        #    alpha_j = self.alpha_j
-        #elif self.type == 'markow':
-        #    alpha_nulla = self.alpha_nulla #RK: Remember you call this function for every agent for every day - why do you set it here? this shall be part of init.
-        #    alpha_j = 1 - alpha_nulla
-        #elif self.type == 'weight':
-        #    alpha_nulla = 0
-        #    alpha_j = self.alpha_j
-        #if self.type == 'weight':
-            #if self.cost[action] != reward: #RK: This is quite dangerous assumption. you beeter check this condition somehow else (what if the costs are the same on two paths?)
-            #    del(self.days[action][len(self.days[action])-1]) #RK: I would try to data structure called queue here https://docs.python.org/3/library/queue.html
-            #    self.days[action].insert(0,self.cost[action])
-            #for i in range(self.remember):
-                    #a = (i+1)**(-1) #RK: This shall be handled via params, see at formulas in Tristan submission and implement accordingly
-            #        c_hat = 0
-            #        c_hat += self.alpha_j[i] * self.days[action][i] #RK: Where here is the loop over paths? you update only a single path ([action])? If so, why? let's discuss.
+    def learn(self, action, observation):
 
-        #else:
-            #print('Correct model definition missing - default markow')
-            #alpha_nulla = self.alpha_nulla
-            #alpha_j = 1 - alpha_nulla
+        reward = self.get_reward(observation)
+        self.last_reward = reward
 
-            #c_hat = self.alpha_nulla * self.cost[action] + self.alpha_j * reward
-
-        #if (c_hat-reward) <= self.lower or (c_hat-reward)>= self.upper:  #RK: normalize and avoid square.
-        if abs(self.old_c_hat[-1]-reward) >= self.gamma_c:
+        if abs(self.old_c_hat[-1]-reward) > self.gamma_c:
 
             if self.type == 'weight':
+                c_hat = 0
                 if self.cost[action] != reward: #RK: This is quite dangerous assumption. you beeter check this condition somehow else (what if the costs are the same on two paths?)
                     del(self.days[action][len(self.days[action])-1]) #RK: I would try to data structure called queue here https://docs.python.org/3/library/queue.html
                     self.days[action].insert(0,self.cost[action])
-                for i in range(self.remember):
-                    #a = (i+1)**(-1) #RK: This shall be handled via params, see at formulas in Tristan submission and implement accordingly
-                    c_hat = 0
-                    c_hat += self.alpha_j[i] * self.days[action][i]
+                for memory in range(self.remember): #memory
 
-                else:
-            #print('Correct model definition missing - default markow')
-            #alpha_nulla = self.alpha_nulla
-            #alpha_j = 1 - alpha_nulla
+                    c_hat += self.alpha_j[memory] * self.days[action][memory]
 
-                    c_hat = self.alpha_nulla * self.cost[action] + self.alpha_j * reward
+            else:
 
-            #self.cost[action] = self.cost[action] #RK: this is redundant
+                    c_hat = self.alpha_zero * self.cost[action] + self.alpha_j * reward
+
             self.cost[action] = c_hat
             self.old_c_hat.append(c_hat)
 
-            #self.cost[action] = self.alpha_nulla * self.cost[action] + self.alpha_j * reward #RK: are you sure you update costs only if the path was chosen?
-        #self.cost[action]=(1-self.alpha) * self.cost[action] + self.alpha * reward
 
-
-    #def calculate_prob(self, utilities, n):
-        #prob = utilities[n] / sum(utilities)
-        #return prob
+    def get_reward(self, observation: list[dict]) -> float:
+        """ This function calculated the reward of each individual agent. """
+        own_tt = -1 * next(obs[kc.TRAVEL_TIME] for obs in observation if obs[kc.AGENT_ID] == self.id) ## Anastasia added the -1
+        return own_tt
     
-
-    def mutate(self):
-        self.mutate_to.q_table = self.cost
-        return self.mutate_to
-    
-    def create_memory(self): #RK: are you sure this shall be in init?
-
-        for i in range(len(self.cost)):
-            for r in range(self.remember):
-                self.days[i].append(self.cost[i])
-
+    #Create noises
     def taste_noise(self):
 
         taste_noise = np.random.gumbel(size=self.action_space)
         
         return taste_noise
 
+    def make_noise(self):
 
+        noises = self.noise_alpha * self.human_noise + (self.noise_taste) * self.route_taste + self.noise_random * np.random.gumbel(size=self.action_space) #RK: This shall be generalized to noise for each actin for each day
+        
+        self.stored_noises = noises
 
+    #Create memory for weighted model
+    
+    def create_memory(self): #RK: are you sure this shall be in init?
 
-
-#class MachineAgent(Agent):
-
-#    def __init__(self, id, start_time, origin, destination, params, action_space_size):
-        super().__init__(id, start_time, origin, destination)
-
-        self.kind = kc.TYPE_MACHINE
-
-        min_alpha, max_alpha = params[kc.MIN_ALPHA], params[kc.MAX_ALPHA]
-        min_epsilon, max_epsilon = params[kc.MIN_EPSILON], params[kc.MAX_EPSILON]
-        min_eps_decay, max_eps_decay = params[kc.MIN_EPS_DECAY], params[kc.MAX_EPS_DECAY]
-
-        self.epsilon = random.uniform(min_epsilon, max_epsilon)
-        self.epsilon_decay_rate = random.uniform(min_eps_decay, max_eps_decay)
-        self.alpha = random.uniform(min_alpha, max_alpha)
-        self.gamma = params[kc.GAMMA]
-
-        self.action_space_size = action_space_size
-        # Q-table assumes only one state, otherwise should be np.zeros((num_states, action_space_size))
-        self.q_table = np.zeros((action_space_size))
-
-
-#    def act(self, state):
-        if np.random.rand() < self.epsilon:    # Explore
-            return np.random.choice(self.action_space_size)
-        else:    # Exploit
-            return np.argmin(self.q_table)
-                
-
-#    def learn(self, action, reward, observation):
-        prev_knowledge = self.q_table[action]
-        self.q_table[action] = prev_knowledge + (self.alpha * (reward - prev_knowledge))
-        self.decay_epsilon()
-
-
-#    def decay_epsilon(self):    # Slowly become deterministic
-        self.epsilon *= self.epsilon_decay_rate
+        for i in range(len(self.action_space)):
+            for r in range(self.remember):
+                self.days[i].append(self.cost[i])
---
+++ data_analysis.py
@@ -0,0 +1,294 @@
+import csv
+import numpy as np
+import os
+import pandas as pd
+
+from scipy.stats import entropy
+
+class Table_record_creator:
+
+    def __init__(self, number_of_episode, folder, day_limit, action_space):
+
+        """
+
+        Create one row for the final table
+
+        number_of_episode: Overall number of episode [int]
+        folder: The name of the folder of the simulation [str]
+        day_limit: The number  of day which should be examined [int]
+        action_space: The number of the action space [int]
+
+
+        """
+
+        self.number_of_episode = number_of_episode +1
+        self.folder = folder
+        self.day_limit = day_limit
+        self.action_space = action_space
+
+        self.check_raw_folder()
+        self.check_raw_folder()
+
+    
+    def table_record(self):
+
+        self.data_creator()
+        data = self.table_record_creator()
+    
+        return data
+    
+    def data_creator(self):
+
+        df = self.Raw_data_frame()
+        df = df[df.day>self.day_limit]
+
+        self.link_data()
+        self.entropy_data(df)
+        self.TT_data(df)
+
+        print('Run was succesful')
+
+    #Creating the raw data frame from the simulation values
+
+    def Concater_to_raw_data(self):
+
+        """
+
+        Creating one joint dataframe from the records day by day
+        
+        """
+
+        if not os.path.exists(f'raws/{self.folder}'):
+            os.makedirs(f'raws/{self.folder}')
+        
+        for i in range(1,self.number_of_episode):
+            act = pd.read_csv(f'training_records/agents/{self.folder}/ep{i}.csv')
+            ep = pd.read_csv(f'training_records/episodes/{self.folder}/ep_ep{i}.csv')
+            df = pd.merge(act,ep,right_on='id',left_on='id')
+            cost = df.cost_table.str.split(pat=',', expand=True)
+            for name in range(self.action_space):
+                cost = cost.rename(columns = {name:f'cost_{name}'})
+            df = df.merge(cost,right_index=True,left_index=True)
+            df = df.drop(columns = ['cost_table'])
+            utility = df.utility.str.split(pat=',', expand=True)
+            for name in range(self.action_space):
+                utility = utility.rename(columns = {name:f'U_{name}'})
+            df = df.merge(utility,right_index=True,left_index=True)
+            df = df.drop(columns = ['utility','kind'])
+            df['day'] = i
+            df.to_csv(f'raws/{self.folder}/raw{i}.csv',index=False)
+        
+        det_main=pd.DataFrame([])
+        for number in range(1,self.number_of_episode):
+            det = pd.read_csv(f'training_records/detector/{self.folder}/detector_ep{number}.csv')
+            det['day'] = number
+            det_main = pd.concat([det_main,det])
+        det_main = det_main.reset_index(drop=True)
+        det_main.to_csv(f'training_records/detector/{self.folder}/all_det.csv',index=False)
+
+    def Raw_data_frame(self,df_final = pd.DataFrame([])):
+        """
+        
+        Create the joint raw data and a folder with all of the raw data
+    
+        """
+        if not os.path.exists('raw_all'):
+            os.makedirs('raw_all')
+
+
+        self.Concater_to_raw_data()
+
+        for i in range(1,self.number_of_episode):
+            df = pd.read_csv(f'raws/{self.folder}/raw{i}.csv')
+            df_ = pd.concat([df_final,df])
+            df_final = df_
+
+
+        df_final['value'] = df_final.apply(lambda row: row[f'cost_{row["action"]}'], axis=1)
+        df_final['label'] = df_final.apply(lambda row: f'cost_{row["action"]}', axis=1)
+        
+        df_final.to_csv(f'raw_all/raw_all_{self.folder}.csv',index=False)
+
+        return df_final
+    
+    #Creating the records for the table record
+    
+    def link_data(self):
+
+        detector = self.detector_std(pd.read_csv(f'training_records/detector/{self.folder}/all_det.csv'))
+
+        with open(f'result/{self.folder}_link.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['name', 'link'])
+            for key, value in detector.items():
+                writer.writerow([key, value])
+            
+        result_link = {}
+
+        df = pd.read_csv(f'result/{self.folder}_link.csv')
+        result_link[self.folder] = df.values[6, 1]
+
+        with open(f'result/link_result.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['name', 'value'])
+            for key, value in result_link.items():
+                writer.writerow([key, value])
+
+        os.remove(f'result/{self.folder}_link.csv')
+
+    def entropy_data(self, df):
+
+        entropy = self.entropy_calculator(df)
+
+        with open(f'result/{self.folder}_entropy.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['name', 'entropy'])
+            for key, value in entropy.items():
+                writer.writerow([key, value])
+
+        result_entropy = {}
+
+        df = pd.read_csv(f'result/{self.folder}_entropy.csv')
+
+        result_entropy[self.folder] = [np.mean(df.values[:, 1]),np.std(df.values[:, 1])]
+
+        with open(f'result/entropy_result.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['name', 'value'])
+            for key, value in result_entropy.items():
+                writer.writerow([key, value])
+        
+        os.remove(f'result/{self.folder}_entropy.csv')
+
+    def TT_data(self, df):
+
+        TravelTime = self.TotalTravelTime(df=df)
+
+        with open(f'result/{self.folder}_TT.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['day', 'TT'])
+            for key, value in TravelTime.items():
+                writer.writerow([key, value])
+
+        result_TT = {}
+
+        df = pd.read_csv(f'result/{self.folder}_TT.csv')
+
+        result_TT[self.folder] = [np.mean(df.values[:, 1]),np.std(df.values[:, 1])]
+
+
+        with open(f'result/TT_result.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['name', 'value'])
+            for key, value in result_TT.items():
+                writer.writerow([key, value])
+        
+        os.remove(f'result/{self.folder}_TT.csv')
+
+    #Creating the final data as a table record
+
+    def table_record_creator(self):
+
+        df = pd.read_csv('result/entropy_result.csv')
+        df.value = df.value.str.replace('[',"")
+        df.value = df.value.str.replace(']',"")
+        entropy = df.name.str.split('_',expand=True)
+        entropy = entropy.rename(columns={0:'network',1:'model',2:'demand',3:'Bounded',4:'Greedy'})
+        entropy1 = df.value.str.split(',',expand=True)
+        entropy1 = entropy1.rename(columns={0:'entropy_value',1:'entropy_std'})
+        entropy = entropy.merge(entropy1,right_index=True,left_index=True)
+
+        df = pd.read_csv('result/link_result.csv')
+        df.value = df.value.str.replace('[',"")
+        df.value = df.value.str.replace("'","")
+        df.value = df.value.str.replace(']',"")
+        link = df.name.str.split('_',expand=True)
+        link = link.rename(columns={0:'network',1:'model',2:'demand',3:'Bounded',4:'Greedy'})
+        link1 = df.value.str.split(',',expand=True)
+        link1 = link1.rename(columns={0:'link_value',1:'link_std'})
+        link = link.merge(link1,right_index=True,left_index=True)
+
+        df = pd.read_csv('result/TT_result.csv')
+        df.name = df.name.str.replace('_TT.csv',"")
+        df.value = df.value.str.replace('[',"")
+        df.value = df.value.str.replace(']',"")
+        TT = df.name.str.split('_',expand=True)
+        TT = TT.rename(columns={0:'network',1:'model',2:'demand',3:'Bounded',4:'Greedy'})
+        TT1 = df.value.str.split(',',expand=True)
+        TT1 = TT1.rename(columns={0:'TT_value',1:'TT_std'})
+        TT = TT.merge(TT1,right_index=True,left_index=True)
+
+        data = link.merge(TT,right_on=['network','model','demand','Bounded','Greedy'],left_on=['network','model','demand','Bounded','Greedy'])
+        data = data.merge(entropy,right_on=['network','model','demand','Bounded','Greedy'],left_on=['network','model','demand','Bounded','Greedy'])
+
+        data.link_value = data.link_value.astype('float').round(0)
+        data.entropy_value = data.entropy_value.astype('float').round(2)
+        data.TT_value = data.TT_value.astype('float').round(0)
+        data.link_std = data.link_std.astype('float').round(2)
+        data.entropy_std = data.entropy_std.astype('float').round(2)
+        data.TT_std = data.TT_std.astype('float').round(2)
+
+        return data
+    
+    #Utilities
+
+    def detector_std(self, df):
+
+        detector = df
+
+        name = detector.detid.unique()
+
+        detector_std = {}
+
+        for n in name:
+
+            detector_std[n] = [np.mean(detector[(detector.detid==n)].flow),np.std(detector[(detector.detid==n)].flow)]
+        
+        return detector_std
+    
+
+    def entropy_calculator(self, df):
+
+        df_sequence = df
+        df_calc = df_sequence
+        df_calc = df_calc.groupby(['origin','destination','id'])['label'].value_counts().unstack(fill_value=0).reset_index()
+        df_calc = df_calc.sort_values('id')
+        keys_to_check = ['cost_1','cost_2', 'cost_3']
+
+        existing_keys = [key for key in keys_to_check if key in df_calc.columns]
+
+        counts = df_calc[existing_keys].values
+
+        entropy_dict = {}
+
+        for i in range(len(counts)):
+            calc = counts[i]
+            total_count = sum(calc)
+            probabilities = [i / total_count for i in calc]
+            entropy_value = entropy(probabilities,base=2)
+            entropy_dict[i] = entropy_value
+        
+        return entropy_dict
+    
+    def TotalTravelTime(self,df):
+
+        days = df.day.unique()
+
+        TT = {} 
+
+        for d in days:
+
+            value = df[df.day==d].sum().reward
+            TT[d] = value
+        
+        return TT
+    
+    def check_result_folder(self):
+    
+        if not os.path.exists(f'result'):
+            os.makedirs(f'result')
+
+    def check_raw_folder(self):
+    
+        if not os.path.exists(f'raws'):
+            os.makedirs(f'raws')
